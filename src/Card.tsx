240a12c0f0ed97034e9e0073d3b043a4baee7b26
ahmedmahovac <amahovac1@etf.unsa.ba>
2022-12-25 05:16:00 +0100

    dodan loader za ucitavanje slika

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
 
 type Props = {
   id: number;
@@ -9,6 +10,8 @@ type Props = {
 };
 
 function Card(props: Props) {
+  const [loading, setLoading] = useState(true); // set some spinner for loading
+
   const { id, title, overview, poster } = props;
 
   const navigate = useNavigate();
@@ -18,11 +21,26 @@ function Card(props: Props) {
     navigate(location.pathname + "/" + id);
   }
 
+  function handleImageLoad() {
+    setLoading(false);
+  }
+
+  function handleImageLoadError() {
+    setLoading(false);
+  }
+
   return (
     <div onClick={handleCardClick} className="Card">
+      {loading ? <div className="loader"></div> : null}
       <img
+        onLoad={handleImageLoad}
+        onError={handleImageLoadError}
         className="ImageCard"
-        src={poster == null ? "" : "https://image.tmdb.org/t/p/w500/" + poster}
+        src={
+          poster == null
+            ? "https://via.placeholder.com/500x753?text=No+Poster"
+            : "https://image.tmdb.org/t/p/w500/" + poster
+        }
         alt="title"
       />
       <h3 className="CardTitle">{title}</h3>
---
+++ src/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+function LoadingSpinner() {
+  return (
+    <div className="Spinner">
+      <h1>Image is loading</h1>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
