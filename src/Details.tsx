306415504e0b7a2daa14c55bb330f5c5a0cbcc93
ahmedmahovac <amahovac1@etf.unsa.ba>
2022-12-26 16:40:23 +0100

    unaprijedjene jos neke stvari

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Video } from "./MovieDetail";
@@ -6,11 +6,13 @@ import { Video } from "./MovieDetail";
 type DetailsProps = {
   poster_path?: string;
   title?: string;
+  name?: string;
   overview?: string;
   video?: Video;
 };
 
 function Details(props: DetailsProps) {
+  const [loading, setLoading] = useState(true);
   const { poster_path, title, overview, video } = props;
   console.log(video?.site);
   const navigate = useNavigate();
@@ -19,37 +21,47 @@ function Details(props: DetailsProps) {
     navigate(-1);
   }
 
+  function handleImageVideoLoadSuccess() {
+    setLoading(false);
+  }
+
+  function handleImageVideoLoadError() {
+    setLoading(false);
+  }
+
   return (
     <div className="detailsContainer">
-      <button type="button" onClick={handleBackClick} className="btnBack">
-        Back
-      </button>
+      <div className="btnBox">
+        <button type="button" onClick={handleBackClick} className="btnBack">
+          Back
+        </button>
+      </div>
       {video ? (
-        <div>
-          <iframe
-            title="trailer video"
-            className="video"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            src={
-              (video.site === "YouTube"
-                ? "https://www.youtube.com/embed/"
-                : "https://vimeo.com/") + video.key
-            }
-          />
-        </div>
+        <iframe
+          title="trailer video"
+          className="video"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          src={
+            (video.site === "YouTube"
+              ? "https://www.youtube.com/embed/"
+              : "//player.vimeo.com/video/") + video.key
+          }
+          onLoad={handleImageVideoLoadSuccess}
+          onError={handleImageVideoLoadError}
+        />
       ) : (
         <img
           src={
             poster_path == null
-              ? ""
+              ? "https://via.placeholder.com/500x753?text=No+Poster"
               : "https://image.tmdb.org/t/p/w400/" + poster_path
           }
           alt="title"
         />
       )}
       <h1>{title}</h1>
-      <h3>{overview}</h3>
+      <p>{overview}</p>
     </div>
   );
 }
